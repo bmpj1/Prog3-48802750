b257d5b54a89519181cc29d128762a354a5efcfc
Brian Mathias, Pesci Juliani <32136708+bmpj1@users.noreply.github.com>
2017-10-05 16:06:18 +0200

    Add files via upload
    Practica version 2.008.

--- src/modelo/EstadoCelda.java
+++ src/modelo/EstadoCelda.java
@@ -1,12 +1,13 @@
 package modelo;//https://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=648:enumerados-como-clases-enum-en-java-constructores-metodo-values-ejercicio-ejemplo-resuelto-cu00682b&catid=68&Itemid=188
 
 public enum EstadoCelda { VIVA("*"), MUERTA(" ");
-	public final String estado;
+	private final String estado;
 	
 	EstadoCelda(String estado) {
 		this.estado = estado;
 	}
-	public String getEstado() {
+	
+	protected String getEstado() {
 		return estado;
 	}
 }
